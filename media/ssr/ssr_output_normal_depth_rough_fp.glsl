c89871e030b7e6409e464f8f64791aeb59b25ec0
Chicho <chichord2005@hotmail.com>
2025-10-05 17:12:09 +0200

    fix: interpolated depth and normals

--- media/ssr/ssr_output_normal_depth_rough_fp.glsl
+++ media/ssr/ssr_output_normal_depth_rough_fp.glsl
@@ -1,11 +1,20 @@
 #version 410
 
+uniform vec3 specular;
+uniform float shininess;
+
 layout(location = 0) in vec3 in_normal_vs;
-layout(location = 1) in float in_depth_ndc01;
-layout(location = 2) in float in_roughness;
+layout(location = 1) in vec4 in_position_cs;
 
 out vec4 out_fragment_color;
 
+
+float luminance_from_rgb(vec3 rgb) {
+    return dot(rgb, vec3(0.2126, 0.7152, 0.0722));
+}
+
 void main() {
-    out_fragment_color = vec4(in_normal_vs.xy, in_depth_ndc01, in_roughness);
+    float roughness = pow(1.0 - luminance_from_rgb(specular), shininess);
+    float depth_ndc01 = in_position_cs.z / in_position_cs.w;
+    out_fragment_color = vec4(normalize(in_normal_vs).xy, depth_ndc01, roughness);
 }
--- media/ssr/ssr_output_normal_depth_rough_vp.glsl
+++ media/ssr/ssr_output_normal_depth_rough_vp.glsl
@@ -14,28 +14,21 @@ uniform mat4 world_view_matrix;
 uniform mat4 it_world_view_matrix;
 uniform mat4 projection_matrix;
 
-uniform vec3 specular;
-uniform float shininess;
-
 layout(location = 0) in vec3 in_position_os;
 layout(location = 2) in vec3 in_normal_os;
 
 layout(location = 0) out vec3 out_normal_vs;
-layout(location = 1) out float out_depth_ndc01;
-layout(location = 2) out float out_roughness;
+layout(location = 1) out vec4 out_position_cs;
+layout(location = 2) out float out_depth_ndc01;
 
-float luminance_from_rgb(vec3 rgb) {
-    return dot(rgb, vec3(0.2126, 0.7152, 0.0722));
-}
 
 void main() {
     vec3 position_os = in_position_os;
 
     vec4 pos_vs = world_view_matrix * vec4(position_os.xyz, 1.0);
     vec4 pos_cs = projection_matrix * vec4(pos_vs.xyz, 1.0);
+    
     gl_Position = pos_cs;
-
+    out_position_cs = pos_cs;
     out_normal_vs = normalize((it_world_view_matrix * vec4(in_normal_os, 0.0)).xyz);
-    out_depth_ndc01 = pos_cs.z / pos_cs.w;
-    out_roughness = pow(1.0 - luminance_from_rgb(specular), shininess);
 }
